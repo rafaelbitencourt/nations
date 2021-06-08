c5c894d81b25c6b2b38910cc4efebbdaacf8acb6
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-06-07 21:23:12 -0300

    Tema em arquivo separado e base url = SRC

---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#60835F'
+        },
+        secondary: {
+            main: '#834949'
+        }
+    }
+});
+
+export default theme;
